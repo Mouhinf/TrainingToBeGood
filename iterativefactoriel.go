68b0eeb732b970cc21f736605a4343894457b553
Mouhinf <mouhopap@gmail.com>
2022-12-27 13:13:05 +0000

    Factorial

--- iterativefactoriel.go
+++ iterativefactoriel.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func IterativeFactorial(nb int) int {
+	f := 1
+	for i := 1; i <= nb; i++ {
+		f = f * i
+	}
+	return f
+}
+
 func main() {
-	fmt.Println("Je fais un testdfj")
+	var nb int
+	fmt.Print("Give the number that you want know its factorial : ")
+	fmt.Scan(&nb)
+	fmt.Println(IterativeFactorial(nb))
 }
