5ba3826d18d562ade5028560867b651016e3af22
Mouhinf <mouhopap@gmail.com>
2022-12-27 19:10:57 +0000

    Fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -6,7 +6,14 @@ func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
 	}
-	return Fibonacci(index-2) - Fibonacci(index-1)
+	if index == 0 {
+		return 0
+	}
+	if index >= 3 {
+		return (Fibonacci(index-1) + Fibonacci(index-2))
+	} else {
+		return 1
+	}
 
 }
 
